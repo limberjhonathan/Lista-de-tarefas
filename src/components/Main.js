9f47a4d03a47171984e73e59b66a3a9af6d6e3f9
Limber Jhonathan <limberjhonathan@gmail.com>
2024-06-09 21:06:13 -0300

    Sua mensagem de commit aqui

---
+++ src/components/Main.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+
+import Form from "./Form";
+import Tarefas from "./Tarefas";
+
+import "./Main.css";
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: "",
+    tarefas: [],
+    index: -1,
+  };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas"));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps);
+    const { tarefas } = this.state;
+
+    // console.log(tarefas, prevState.tarefas);
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1 || !novaTarefa) return;
+
+    const novaTarefas = [...tarefas];
+
+    if (index == -1) {
+      this.setState({
+        tarefas: [...novaTarefas, novaTarefa],
+        novaTarefa: "",
+      });
+    } else {
+      novaTarefas[index] = novaTarefa;
+
+      console.log(novaTarefa);
+
+      this.setState({
+        tarefas: [...novaTarefas],
+        index: -1,
+        novaTarefa: "",
+      });
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novaTarefas = [...tarefas];
+    novaTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novaTarefas],
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
+
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
